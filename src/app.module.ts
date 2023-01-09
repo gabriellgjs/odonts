396faf1b8d56459f4b223fe708d8de7e4eb28659
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-01-09 20:58:44 -0300

    chore(auth): adicionando os arquivos de autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DataBaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule, DataBaseModule],
+  imports: [HttpModule, DataBaseModule, AuthModule],
 })
 export class AppModule {}
